584a3cd88eb8646da60f1d108da426e0a22ec57d
Bhaaliver <oliver@warmbowski.org>
2025-06-12 10:18:10 -0700

    added comments

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -1,24 +1,30 @@
-//sorting method is a modified is at the end of the file and is called sortInsertion
+// LinkedList class implements a singly linked list with insertion sort
 public class LinkedList {
+    // Head node of the list
     private Node head;
+    // Tail node of the list
     private Node tail;
 
+    // Default constructor initializes an empty list
     LinkedList(){
         this.head = null;
         this.tail = null;
     }
 
+    // Constructor initializes the list with a given head node
     LinkedList(Node head){
         this.head = head;
         this.tail = head;
     }
 
+    // Constructor initializes the list with a single integer data node
     LinkedList(int data){
         Node newNode = new Node(data);
         this.head = newNode;
         this.tail = newNode;
     }
 
+    // Adds a new node with the given data to the end of the list
     public void addNode(int data){
         if (head == null) {
             head = new Node(data);
@@ -27,10 +33,12 @@ public class LinkedList {
         }
         Node newNode = new Node(data);
         tail.setNextNode(newNode);
+        // Move tail to the new last node
         while (tail.getNextNode() != null)
             tail = tail.getNextNode();
     }
 
+    // Adds an existing node to the end of the list
     public  void addNode(Node newNode){
         if (head == null) {
             head = newNode;
@@ -38,10 +46,12 @@ public class LinkedList {
             return;
         }
         tail.setNextNode(newNode);
+        // Move tail to the new last node
         while(tail.getNextNode() != null)
             tail = tail.getNextNode();
     }
 
+    // Inserts a node after a given parent node
     public void addNode(Node newNode, Node newParent){
         Node oldChild = newParent.getNextNode();
         if (oldChild == null){
@@ -51,17 +61,21 @@ public class LinkedList {
         newParent.setNextNode(newNode);
     }
 
+    // Returns the head node of the list
     public Node getHead() {
         return head;
     }
 
+    // Returns the tail node of the list
     public Node getTail(){
         return tail;
     }
 
+    // Returns the length of the list
     public int getLength(){
         int length = 0;
         Node pointer = head;
+        // Traverse the list and count nodes
         while(pointer != null){
             length++;
             pointer = pointer.getNextNode();
@@ -69,6 +83,7 @@ public class LinkedList {
         return length;
     }
 
+    // Sorts the linked list using insertion sort algorithm
     public void sortInsertion(){
         // Initialize sortedHead with the first node of the list
         Node sortedHead = head;
@@ -104,8 +119,9 @@ public class LinkedList {
                 }
             }
         }
-        // Update head and tail references
+        // Update head to the new sorted head
         head = sortedHead;
+        // Update tail to the last node
         while(tail.getNextNode() != null)
             tail = tail.getNextNode();
     }
